11d6aeeb2951fb00d979819e5f3fd8ded6d3fb23
Romelec <dev@romelec.fr>
2023-07-02 22:25:20 +0200

    first commit

---
+++ scheduler.py
@@ -0,0 +1,152 @@
+import json
+import schedule
+import datetime, time, re
+import threading
+
+import broadlink
+from broadlink.const import DEFAULT_PORT
+from broadlink.exceptions import ReadError, StorageError
+
+import web
+
+json_data_file = "data.json"
+json_jobs_file = "jobs.json"
+
+scheduler_thread = None
+scheduler_thread_event = None
+json_data = None
+device = None
+
+def setup(json_data):
+    for itm in json_data:
+        if itm['name'] == "device":
+            devtype = int(itm['devtype'], 0)
+            host = itm['host']
+            mac = bytearray.fromhex(itm['mac'])
+
+            print(f"Setup {devtype}, {host}, {mac}")
+            dev = broadlink.gendevice(devtype, (host, DEFAULT_PORT), mac)
+            dev.auth()
+            return dev
+
+    print ("Setup data not found")
+    return None
+
+def get_signal(action):
+    for itm in json_data:
+        if itm['name'] == action:
+            data = bytearray.fromhex(''.join(itm['data'])) 
+            return data
+
+    print (f"Data not found for '{action}'")
+    return None
+
+def send_single (action):
+    if (not action):
+        print("Nothing to do")
+        return
+
+    data = get_signal(action)
+    print (f"Send {action}")
+    if data != None:
+        device.send_data(data)
+
+def send_irdata(job_name, job_param):
+    print(f"Running '{job_name}' with parameters: {job_param}")
+    action1 = re.split(r',\s*', job_param['action1'])
+    action2 = re.split(r',\s*', job_param['action2'])
+    delay = job_param['delay']
+    weekday = job_param['weekday']
+    weekend = job_param['weekend']
+
+    # Execute depending of the day
+    week = datetime.datetime.today().isoweekday()
+    if (weekday and week <= 5) or (weekend and week > 5):
+        # action 1
+        for action in action1:
+            send_single(action)
+            time.sleep(0.5)
+        # pause
+        print(f"Pause {delay}s")
+        time.sleep(delay)
+        # action 2
+        for action in action2:
+            send_single(action)
+            time.sleep(0.5)
+
+def read_data_from_json(json_file):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    return data
+
+def schedule_jobs(jobs):
+    for job in jobs:
+        # Schedule the job with the specified time and parameters
+        job_name = job['name']
+        job_time = job['time']
+        job_param = job.get('parameters', None)
+
+        print(f"Schedule job '{job_name}' at {job_time}: '{job_param}'")
+        schedule.every().day.at(job_time).do(send_irdata, job_name, job_param)
+
+    # Restart scheduler every morning
+    schedule.every().day.at("07:00").do(config_update)
+
+def run_scheduler(interval=1):
+    thread_event = threading.Event()
+
+    class ScheduleThread(threading.Thread):
+        @classmethod
+        def run(cls):
+            global json_data
+            global device
+
+            # Read data file and configure the device
+            json_data = read_data_from_json(json_data_file)
+            device = setup(json_data)
+
+            # Read jobs and configure scheduler
+            jobs = read_data_from_json(json_jobs_file)
+            schedule_jobs(jobs)
+
+            # Run scheduler
+            while not thread_event.is_set():
+                schedule.run_pending()
+                time.sleep(interval)
+            
+            # Cancel all jobs before exiting
+            schedule.clear()
+            print("Scheduler stopped")
+
+    thread = ScheduleThread()
+    thread.start()
+    return thread, thread_event
+
+def config_update():
+    print("update config -> restart scheduler thread")
+    global scheduler_thread
+    global scheduler_thread_event
+    # Stop the background thread
+    scheduler_thread_event.set()
+    scheduler_thread.join()
+
+    # Start the background thread
+    scheduler_thread, scheduler_thread_event = run_scheduler()
+
+def main():
+    global scheduler_thread
+    global scheduler_thread_event
+
+    # Start the scheduler thread
+    scheduler_thread, scheduler_thread_event = run_scheduler()
+
+    # Start the webserver
+    web.start(config_update)
+
+    # wait forever
+    while(1):
+        time.sleep(100)
+
+
+if __name__ == "__main__":
+    main()
---
+++ web.py
@@ -0,0 +1,105 @@
+from flask import Flask, request, jsonify, render_template
+import json, time
+import threading
+
+app = Flask(__name__)
+
+# Define the JSON file name
+json_file = "jobs.json"
+
+# Callback when a job is updated
+job_update_cb = None
+
+# Load the initial jobs from the JSON file
+with open(json_file, 'r') as f:
+    jobs_data = json.load(f)
+
+def save_jobs_to_json(jobs):
+    # Save the updated jobs to the JSON file
+    with open(json_file, 'w') as f:
+        json.dump(jobs, f, indent=4)
+
+@app.route('/')
+def home():
+    # reload data from file
+    with open(json_file, 'r') as f:
+        jobs_data = json.load(f)
+    # Render the home page with the list of jobs
+    return render_template('index.html', jobs=jobs_data)
+
+@app.route('/add_job', methods=['POST'])
+def add_job():
+    print(f"request {request.form}")
+    print(f"check weekday {request.form.getlist('weekday')}-{bool(request.form.getlist('weekday'))}, weekend {request.form.getlist('weekend')}-{bool(request.form.getlist('weekend'))}")
+    # Parse job details from the request
+    job_name = request.form['name']
+    job_time = request.form['time']
+    job_parameters = {
+        "action1": request.form['action1'],
+        "delay": int(request.form['delay']),  # Convert to integer
+        "action2": request.form['action2'],
+        "weekday": bool(request.form.getlist('weekday')),
+        "weekend": bool(request.form.getlist('weekend')),
+    }
+
+    # Check if a job with the same name already exists
+    existing_job = next((job for job in jobs_data if job['name'] == job_name), None)
+
+    if existing_job:
+        # If the job exists, update its time and parameters
+        existing_job['time'] = job_time
+        existing_job['parameters'] = job_parameters
+    else:
+        # If the job does not exist, add it to the jobs_data list
+        new_job = {
+            'name': job_name,
+            'time': job_time,
+            'parameters': job_parameters
+        }
+        jobs_data.append(new_job)
+
+    # Save the updated jobs to the JSON file
+    save_jobs_to_json(jobs_data)
+
+    if job_update_cb != None:
+        job_update_cb()
+
+    # Redirect to the home page after adding the job
+    return home()
+
+@app.route('/remove_job', methods=['POST'])
+def remove_job():
+    # Parse the job name from the request
+    job_name = request.form['name']
+
+    # Remove the job from the jobs_data list
+    jobs_data[:] = [job for job in jobs_data if job['name'] != job_name]
+
+    # Save the updated jobs to the JSON file
+    save_jobs_to_json(jobs_data)
+
+    if job_update_cb != None:
+        job_update_cb()
+
+    # Redirect to the home page after removing the job
+    return home()
+
+def web_thread(callback = None):
+    print(f"set callback {callback}")
+    if callback != None:
+        global job_update_cb
+        job_update_cb = callback
+
+    app.run(host='0.0.0.0', port=8080)
+
+def start(callback = None):
+    thread = threading.Thread(target=web_thread, args=(callback,))
+    thread.start()
+
+if __name__ == "__main__":
+    
+    web_thread()
+
+    #start(cb)
+    while(1):
+        time.sleep(10)
